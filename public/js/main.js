efe1cf2922f202f5e7dff91e8319d9702c78d683
Adrian Kulawik <a.kulawik8@gmail.com>
2025-01-04 15:59:55 +0100

    Refaktoryzacja kodu

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
-function switch_menu() {
-	var x = document.getElementById("left-nav-id");
+function switch_left_bar() {
+	var x = document.getElementById("left-bar-id");
 	if (x.style.display === "none" || x.style.display === "") {
-	  x.style.display = "flex";
+		x.style.display = "flex";
 	} else {
-	  x.style.display = "none";
+		x.style.display = "none";
 	}
-  } 
+}
